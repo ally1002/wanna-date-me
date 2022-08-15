f5bb327880a9de6a7a3e5a3b18a3d38bab3812f7
aly <alyssonrosa1@gmail.com>
2022-08-15 20:23:58 -0300

    done

--- src/components/WannaDateMe.jsx
+++ src/components/WannaDateMe.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import '../css/WannaMarryMe.css';
 
 function WannaDateMe() {
+
+    useLayoutEffect(() => {
+        replace();
+    })
+
+    const replace = () => {
+        const noButton = document.getElementById('no');
+        noButton.style.left = `${Math.floor(Math.random() * 80)}%`;
+        noButton.style.top = `${Math.floor(Math.random() * 90) + 5}%`;
+    }
+
+    const gotIt = () => {
+        document.querySelector('.app').innerHTML = `
+            <h1 class="wanna">love you ❤</h1>
+        `;
+    }
+
     return (
         <div className="app">
             <h1 className="wanna">Quer namorar comigo?</h1>
-            <button>Sim</button>
-            <button>Não</button>
+            <button
+                className="button"
+                id="yes"
+                onClick={gotIt}
+            >Sim ❤
+            </button>
+            <button
+                className="button"
+                id="no"
+                onClick={replace}
+            >Não
+            </button>
         </div>
     )
 }
